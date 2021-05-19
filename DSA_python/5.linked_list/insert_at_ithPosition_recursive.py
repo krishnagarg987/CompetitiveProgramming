099da77e9f540ac65de8a91d7feebb2ab3373783
Krishna Agrawal <56031994+krxxnna@users.noreply.github.com>
2021-05-19 21:58:05 +0530

    Update insert_at_ithPosition_recursive.py

--- DSA_python/5.linked_list/insert_at_ithPosition_recursive.py
+++ DSA_python/5.linked_list/insert_at_ithPosition_recursive.py
@@ -23,7 +23,23 @@ def length(head):
     while head.next!=None:
         c+=1
         head=head.next
-    return c+1
+    return c+1  
+        
+def insert(head1,position,data):
+    if position>(l+1):
+        return head1
+    newnode=node(data)
+    head=head1
+    if position==1:
+        newnode.next=head
+        head=newnode
+        return head
+    else:    
+        for i in range(position-2):
+            head=head.next
+        newnode.next=head.next
+        head.next=newnode
+        return head1
 
 def printll(head):
     while head!=None:
@@ -31,22 +47,10 @@ def printll(head):
         head=head.next
     print(None)
     
-def insert(head,position,data):
-    if position>(l+1):
-        return head
-    newnode=node(data)
-    if position==1:
-        newnode.next=head
-        head=newnode
-        return head
-    smalloutput=insert(head.next,position-1,data)
-    head.next=smalloutput
-    return head
-    
 a=[1,2,3,4,5,6,7,8,-1]  
 head=linkedlist(a)
 printll(head)
 # insert(head,position,data)
 l=length(head)
 head=insert(head,1,9)
-printll(head)    
+printll(head)
