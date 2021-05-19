a12cc17bfe7dfa38145f66329db9cb7d208b3047
Krishna Agrawal <56031994+krxxnna@users.noreply.github.com>
2021-05-19 15:26:51 +0530

    Create create&print_linkedList.py

---
+++ DSA_python/5.linked_list/create&print_linkedList.py
@@ -0,0 +1,35 @@
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        
+def linkedlist(a):
+    head=None
+    current=None
+    for i in a:
+        if i==-1:
+            break
+        newnode=node(i)
+        if head is None:
+            head=newnode
+            current=head
+        else:
+            current.next=newnode
+            current=current.next
+    return head
+def printll(head):
+    while head!=None:
+        print(head.data,'-> ',end="")
+        head=head.next
+    print(None)
+    
+a=[1,2,3,4,5,6,7,8,-1]  
+head=linkedlist(a)
+printll(head)
+
+
+
+
+
+
+
