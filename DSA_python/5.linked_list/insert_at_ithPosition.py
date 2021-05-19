39dec1a910e906d1bbb8f3c2ce35d7203bddf766
Krishna Agrawal <56031994+krxxnna@users.noreply.github.com>
2021-05-19 19:21:48 +0530

    Create insert_at_ithPosition.py

---
+++ DSA_python/5.linked_list/insert_at_ithPosition.py
@@ -0,0 +1,63 @@
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        
+def linkedlist(a):
+    head=None
+    current=None
+    for i in a:
+        if i==-1:
+            break
+        newnode=node(i)
+        if head is None:
+            head=newnode
+            current=head
+        else:
+            current.next=newnode
+            current=current.next
+    return head
+
+def length(head):
+    c=0
+    while head.next!=None:
+        c+=1
+        head=head.next
+    return c    
+        
+def insert(head1,position,data):
+    if position<1 or position>l:
+        return head1
+    newnode=node(data)
+    head=head1
+    if position==1:
+        newnode.next=head
+        head=newnode
+        return head
+    else:    
+        for i in range(position-2):
+            head=head.next
+        newnode.next=head.next
+        head.next=newnode
+        return head1
+
+def printll(head):
+    while head!=None:
+        print(head.data,'-> ',end="")
+        head=head.next
+    print(None)
+    
+a=[1,2,3,4,5,6,7,8,-1]  
+head=linkedlist(a)
+printll(head)
+# insert(head,position,data)
+l=length(head)
+head=insert(head,9,9)
+printll(head)
+
+
+
+
+
+
+
