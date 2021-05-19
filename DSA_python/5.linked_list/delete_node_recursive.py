dd90d8552790bbdce3aef0b8fe69df9adb18251a
Krishna Agrawal <56031994+krxxnna@users.noreply.github.com>
2021-05-20 01:08:37 +0530

    Create delete_node_recursive.py

---
+++ DSA_python/5.linked_list/delete_node_recursive.py
@@ -0,0 +1,50 @@
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        
+def linkedlist(a):
+    head=None
+    current=None
+    for i in a:
+        if i==-1:
+            break
+        newnode=node(i)
+        if head is None:
+            head=newnode
+            current=head
+        else:
+            current.next=newnode
+            current=current.next
+    return head
+
+def length(head):
+    c=0
+    while head.next!=None:
+        c+=1
+        head=head.next
+    return c+1
+
+def printll(head):
+    while head!=None:
+        print(head.data,'-> ',end="")
+        head=head.next
+    print(None)
+
+def deleteNode(head,position):
+    if position<1 or position>l:
+        return head
+    head1=head
+    if position==1:
+        head=head.next
+        return head
+    smalloutput=deleteNode(head.next,position-1)
+    head1.next=smalloutput
+    return head1
+
+a=[1,2,3,4,5,6,7,8,-1]  
+head=linkedlist(a)
+l=length(head)
+head=deleteNode(head,0)
+printll(head)
+
