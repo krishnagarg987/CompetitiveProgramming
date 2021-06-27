d29a60f509c7e136d3e0d552c88866f363146307
krxxnna <krishnagarg987@gmail.com>
2021-06-27 09:42:04 +0530

    Created

--- DSA_python/15. DP/LCS.py
+++ DSA_python/15. DP/LCS.py
@@ -1,29 +1,31 @@
-def lcs(s1,s2):
-    if s1=="" or s2=="":
+def lcs(s1,s2,i,j):
+    if i==len(s1) or j==len(s2):
         return 0
-    if s1[0]==s2[0]:
-        ans=0
-        if not dp.get(s1[1:]+s2[1:],False):
-            ans=lcs(s1[1:],s2[1:])
-            dp[s1[1:]+s2[1:]]=ans
+    if s1[i]==s2[j]:
+        if dp[i+1][j+1]==-1:
+            ans=lcs(s1,s2,i+1,j+1)
+            dp[i+1][j+1]=ans
         else:
-            ans=dp[s1[1:]+s2[1:]]
-        dp[s1+s2]=ans+1
-    if s1[0]!=s2[0]:
-        ans1=ans2=0
-        if not dp.get(s1+s2[1:],False):
-            ans1=lcs(s1, s2[1:])
-            dp[s1+s2[1:]]=ans1
+            ans=dp[i+1][j+1]
+        dp[i][j]=ans+1
+    else:
+        if dp[i][j+1]==-1:
+            ans1=lcs(s1,s2,i,j+1)
+            dp[i][j+1]=ans1
         else:
-            ans1=dp[s1+s2[1:]]
-        if not dp.get(s1[1:]+s2,False):
-            ans2=lcs(s1[1:],s2)
-            dp[s1[1:]+s2]=ans2
+            ans1=dp[i][j+1]
+        if dp[i+1][j]==-1:
+            ans2=lcs(s1,s2,i+1,j)
+            dp[i+1][j]=ans2
         else:
-            ans2=dp[s1[1:]+s2]
-        dp[s1+s2]=max(ans1,ans2)
-    return dp[s1+s2]
+            ans2=dp[i+1][j]
+        dp[i][j]=max(ans1,ans2)
+    return dp[i][j]
 
-dp={}
-print(lcs("abdgec","bfdmgjc"))
-print(len(dp))
+s1="abd"
+s2="bfd"
+n=len(s1)
+m=len(s2)
+dp=[[-1 for i in range(m+1)] for j in range(n+1)]
+print(lcs(s1,s2,0,0))
+print(dp)
